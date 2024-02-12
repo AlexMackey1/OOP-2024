fae703f2c5df49300cb1b9512c6e2360b7aeb658
AlexMackey1 <C22739165@mytudublin.ie>
2024-02-12 11:57:02 +0000

    Practicing arrays yipeeeee

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -5,9 +5,52 @@ import processing.core.PApplet;
 public class Arrays extends PApplet
 {
 
+	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+	float[] rainfall = {200, 260, 300, 150, 100, 50, 10, 40, 67, 160, 400, 420};
+
 	public void settings()
 	{
 		size(500, 500);
+
+		String[] m1 = months;
+		months[0] = "XXX";
+		print(m1[0]);
+
+		float high = rainfall[0];
+		float low = rainfall[0];
+		String high_month = " ";
+		String low_month = " ";
+
+		
+		for(int i = 0; i < months.length; i++)
+		{
+			println("Month: " + months[i] + "\t" + rainfall[i]);
+		}
+
+		for (String s : m1)
+		{
+			println(s);
+		}
+
+		for(int i = 0; i < months.length; i++)
+		{
+			if (high < rainfall[i])
+			{
+				high = rainfall[i];
+				high_month = months[i];
+
+			}
+
+			if (low > rainfall[i])
+			{
+				low = rainfall[i];
+				low_month = months[i];
+			}
+		}
+
+		println("Highest rainfall: ", high_month, "with", high, "mm");
+		println("Lowest rainfall: ", low_month, "with", low, "mm");
 	}
 
 	public void setup() {
